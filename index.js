5dc6bde24a67d172f8887dfc735ac5a8950a9c6a
kiwaeva <kiwaeva@gmail.com>
2025-11-15 12:07:53 +0000

    Add js file and write 4 functions: add, subtract, multiply and divide.

---
+++ index.js
@@ -0,0 +1,15 @@
+let add = (a,b) => {
+    return a + b
+};
+
+let subtract = (a,b) => {
+    return a - b
+};
+
+let multiply = (a,b) => {
+    return a * b
+};
+
+let divide = (a,b) => {
+    return a / b ;
+}
