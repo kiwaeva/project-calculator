fbf7344862f17021b11fdaa4440ba6b59fc95d6b
kiwaeva <kiwaeva@gmail.com>
2025-11-19 13:22:56 +0000

    Write operate function.

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ let multiply = (a,b) => {
 let divide = (a,b) => {
     return a / b ;
 }
+
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+
+let operate = (operator,a,b) => {
+    if (operator === '+'){
+        return add(a,b)
+    } if (operator === '-'){
+        return subtract(a,b)
+    } if ( operator==='*'){
+        return multiply(a,b)
+    }if (operator === '/' ){
+        return divide(a,b)
+    }
+}
