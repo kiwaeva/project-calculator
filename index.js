08ad8bccd0ae1faf4b5082f6c1135ff5787ac29b
kiwaeva <kiwaeva@gmail.com>
2025-11-21 15:39:55 +0000

    Clean up the code and format documnet.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+let add = (a, b) => {
+    return a + b;
+};
+
+let subtract = (a, b) => {
+    return a - b;
+};
+
+let multiply = (a, b) => {
+    return a * b;
+};
+
+let divide = (a, b) => {
+    return a / b;
+};
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+
+let operate = (operator, a, b) => {
+    if (operator === '+') {
+        return add(a, b);
+    } if (operator === '-') {
+        return subtract(a, b);
+    } if (operator === '*') {
+        return multiply(a, b);
+    } if (operator === '/') {
+        return divide(a, b);
+    }
+};
+
 let display = document.getElementsByClassName("screen")[0];
 let buttons = document.getElementsByClassName("btn");
 let displayValue = "";
@@ -6,44 +38,11 @@ let displayValue = "";
 let populate = (number) => {
     displayValue += number;
     display.textContent = displayValue;
-}
-
-for(let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click',(event) => {
-    let number = event.target.textContent;
-    populate(number);
-})
 };
 
-let add = (a,b) => {
-    return a + b
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (event) => {
+        let number = event.target.textContent;
+        populate(number);
+    })
 };
-
-let subtract = (a,b) => {
-    return a - b
-};
-
-let multiply = (a,b) => {
-    return a * b
-};
-
-let divide = (a,b) => {
-    return a / b ;
-}
-
-
-let firstNum = '';
-let secondNum = '';
-let operator = '';
-
-let operate = (operator,a,b) => {
-    if (operator === '+'){
-        return add(a,b)
-    } if (operator === '-'){
-        return subtract(a,b)
-    } if ( operator==='*'){
-        return multiply(a,b)
-    }if (operator === '/' ){
-        return divide(a,b)
-    }
-}
