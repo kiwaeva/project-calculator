80fefa830b05c630f2c099420fbe37e33cd3ffd3
kiwaeva <kiwaeva@gmail.com>
2025-11-25 20:10:02 +0000

    Write a code to detect buttons values and calculate the result as well as reset the calculator.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let firstNum = '';
 let secondNum = '';
 let operator = '';
 let displayValue = '';
+
 let display = document.getElementsByClassName("screen")[0];
 let buttons = document.getElementsByClassName("btn");
 
@@ -38,14 +39,40 @@ let operate = (operator, a, b) => {
 
 let populate = (number) => {
     displayValue += number;
-    display.textContent = displayValue;
+    display.value = displayValue;
 };
 
 
 //Event listeners
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', (event) => {
-        let number = event.target.textContent;
-        populate(number);
-    })
+        let value = event.target.textContent; // targets the text content within the buttons element 
+
+    // Detect button values
+        if (!isNaN(value)) {
+            populate(value);
+        } else if (value === '+' || value === '-' || value === '*' || value === '/'){
+            firstNum = displayValue;
+            operator = value;
+            displayValue  = ''; 
+            display.textContent = '';
+        } else if(value === '='){
+            secondNum = displayValue;
+            let result = (operate(operator, Number(firstNum), Number(secondNum)));
+            displayValue = result;
+            display.value = displayValue; //.value for input fiels otherwise .textContent
+
+            //store result as a firstNum for the next calculation
+            firstNum = result; 
+        } else if (value === "AC"){
+            //reset all of the values 
+            displayValue = '';
+            firstNum = '';
+            secondNum = '';
+            operator = '';
+            display.value = displayValue;
+        }
+    });
 };
+
+//store values 
